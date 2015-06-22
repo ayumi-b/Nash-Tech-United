d878c32ac4a3c6b0ef83a540897f0589c9ec45e6
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-06-22 14:48:59 -0500

    added action model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621144307) do
+ActiveRecord::Schema.define(version: 20150622175322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "goal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "actions", ["user_id"], name: "index_actions_on_user_id", using: :btree
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -51,5 +60,6 @@ ActiveRecord::Schema.define(version: 20150621144307) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "actions", "users"
   add_foreign_key "identities", "users"
 end
