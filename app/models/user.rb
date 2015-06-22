c09138b0d0d1b68d806dd206c4ff4a964b5c576c
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-06-22 05:13:26 -0500

    adding omniauth paths into the routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable
+  has_many :identities
+
+  def twitter
+    identities.where( :provider => "twitter" ).first
+  end
+
+  def twitter_client
+    @twitter_client ||= Twitter.client( access_token: twitter.accesstoken )
+  end
+
 end
