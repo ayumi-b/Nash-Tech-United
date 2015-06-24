6b5447cd0dbc8de94b5a60c577747001dde17e3f
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-06-24 05:51:45 -0500

    added devise test help config

---
+++ spec/fabricators/user_fabricator.rb
@@ -0,0 +1,16 @@
+Fabricator(:user) do
+  id                     1
+  email                  "MyString"
+  encrypted_password     "MyString"
+  reset_password_token   "MyString"
+  reset_password_sent_at "2015-06-24 05:37:41"
+  remember_created_at    "2015-06-24 05:37:41"
+  sign_in_count          1
+  current_sign_in_at     "2015-06-24 05:37:41"
+  last_sign_in_at        "2015-06-24 05:37:41"
+  current_sign_in_ip     ""
+  last_sign_in_ip        ""
+  created_at             "2015-06-24 05:37:41"
+  updated_at             "2015-06-24 05:37:41"
+  name                   "MyString"
+end
