d20716f23616b7bf0e66a33a4f22513c4f22d455
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-06-24 05:11:18 -0500

    some user sign up tests passing

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -5,13 +5,32 @@ feature "User Signs Up" do
     click_on "I'll Take the Pledge!"
   end
 
-  scenario "Happy Path With No Bio" do
+  scenario "Happy Path With Sign Up" do
     page.should_not have_link("Sign Up")
     fill_in "Name", with: "Kit"
     fill_in "Email", with: "kit@kat.com"
     fill_in "Password", with: "password1"
     fill_in "Password confirmation", with: "password1"
     click_button "Sign up"
-    visit new_user_action_path(:id => @user_id)
+    page.should have_content("You have signed up successfully.")
+  end
+
+  scenario "Error Path" do
+    fill_in "Name", with: ""
+    fill_in "Email", with: "joeexample.com"
+    fill_in "Password", with: "password1"
+    fill_in "Password confirmation", with: "food"
+    click_on "Sign up"
+    # page.should have_notification("Please review the problems below:")
+
+    page.should have_error("is invalid", on: "Email")
+    page.should have_error("doesn't match Password", on: "Password confirmation")
+
+    fill_in "Name", with: "Sally"
+    fill_in "Email", with: "joe@example.com"
+    fill_in "Password", with: "password1"
+    fill_in "Password confirmation", with: "password1"
+    click_on "Sign up"
+    page.should have_content("You have signed up successfully.")
   end
 end
